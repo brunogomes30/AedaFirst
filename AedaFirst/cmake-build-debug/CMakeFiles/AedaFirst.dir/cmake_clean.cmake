d69d8fa94a714edc2be62d3f0b4f45d7ef70fe71
Bruno Gomes <bruno.camgomes@gmail.com>
2020-10-24 14:36:24 +0100

    Empty project

---
+++ AedaFirst/cmake-build-debug/CMakeFiles/AedaFirst.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AedaFirst.exe"
+  "AedaFirst.exe.manifest"
+  "AedaFirst.pdb"
+  "CMakeFiles/AedaFirst.dir/main.cpp.obj"
+  "libAedaFirst.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AedaFirst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AedaFirst/cmake-build-debug/CMakeFiles/AedaFirst.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AedaFirst.
+# This may be replaced when dependencies are built.
