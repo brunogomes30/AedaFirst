c617e6edfea66647731258e6f94acb609d24dce9
Henrique Nunes <hrn2001@gmail.com>
2020-10-29 10:58:29 +0000

    Novos métodos e atributos de store

--- AedaFirst/store.cpp
+++ AedaFirst/store.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 Store::Store(std::string name, Address address) {
-  this->name = name;
-  this->address = address;
+    this->id = store_next_id++;
+    this->name = name;
+    this->address = address;
+}
+
+unsigned Store::getId() const {
+    return id;
 }
 
 string Store::getName() const {
@@ -14,11 +19,32 @@ Address Store::getAddress() const {
   return address;
 }
 
+Product* Store::getProduct(unsigned id) const {
+    for (auto product:products) {
+        if (product->getId() == id)
+            return product;
+    }
+}
+
 void Store::setName(std::string name) {
   this->name = name;
 }
 
 void Store::setAddress(Address address) {
   this->address = address;
+}
+
+void Store::addProduct(Product *product) {
+    products.push_back(product);
+}
+
+bool Store::operator==(const Store &store) const {
+    return id == store.getId();
+}
+
+void Store::showProducts() const {
+    for (auto product:products){
+        cout << product->getId() << " -> " << product->getName() << " " << product->getPrice() << "€" << endl;
+    }
 }
 
--- AedaFirst/store.h
+++ AedaFirst/store.h
@@ -2,22 +2,33 @@
 #define STORE_H
 
 #include <iostream>
+#include <vector>
+#include "product.h"
 
 struct Address{
   std::string street;
   std::string locality;
 };
 
+static unsigned store_next_id = 1;
+
 class Store {
 private:
+    unsigned id;
     std::string name;
     Address address;
+    std::vector<Product*> products;
 public:
     Store(std::string name, Address address);
+    unsigned getId() const;
     std::string getName() const;
     Address getAddress() const;
+    Product* getProduct(unsigned id) const;
     void setName(std::string name);
     void setAddress(Address address);
+    void addProduct(Product *product);
+    bool operator==(const Store &store) const;
+    void showProducts() const;
 };
 
 
