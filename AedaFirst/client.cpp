4fc1ce241df04359e58310093cb649030dd2e478
Henrique Nunes <hrn2001@gmail.com>
2020-10-29 10:57:14 +0000

    Método makeOrder() simplesmente implementado e pronto para criar objeto Venda

--- AedaFirst/client.cpp
+++ AedaFirst/client.cpp
@@ -1,4 +1,5 @@
 #include "client.h"
+#include <algorithm>
 using namespace std;
 
 Client::Client() {
@@ -6,6 +7,7 @@ Client::Client() {
     nif = 0;
     regime = false;
     points = 0;
+    discount = false;
     sumOfAppraisals = 0;
 }
 
@@ -14,6 +16,7 @@ Client::Client(std::string name, unsigned int nif, bool regime) {
     this->nif = nif;
     this->regime = regime;
     points = 0;
+    discount = false;
     sumOfAppraisals = 0;
 }
 
@@ -24,6 +27,8 @@ unsigned Client::getNif() const {return nif;}
 bool Client::getRegime() const {return regime;}
 
 unsigned Client::getPoints() const {return points;}
+
+bool Client::getDiscount() const {return discount;}
 
 bool Client::getOpinion() const {return opinion;}
 
@@ -40,4 +45,71 @@ void Client::addAppraisal(int appraisal) {
         opinion = true; // positive opinion
     else
         opinion = false; // negative opinion
+}
+
+void Client::addPoints(const float &bill) {
+    points += (int) bill * 10;
+
+    if (regime)
+        if (points >= 100) {
+            points = 0;
+            discount = true;
+        }
+    else
+        if (points >= 200) {
+            points = 0;
+            discount = true;
+        }
+
+}
+
+void Client::makeOrder(vector<Store*> stores) {
+    Store* s;
+    vector<Product*> products;
+    vector<int> quantities;
+    unsigned appraisal, store_id, product_id, qnt;
+    float bill=0;
+
+    // Choose store
+    for (auto store:stores){
+        cout << store->getId() << " -> " << store->getName() << endl;
+    }
+    cin >> store_id;
+    for (auto store:stores){
+        if (store_id == store->getId())
+            s = store;
+    }
+
+    // Choose products
+    s->showProducts();
+    do {
+        cout << "Product you want: ";
+        cin >> product_id;
+        products.push_back(s->getProduct(product_id));
+        cout << "How many: ";
+        cin >> qnt;
+        quantities.push_back(qnt);
+    } while (product_id != 0);
+
+    // Bill
+    for (size_t i = 0; i < quantities.size(); i++){
+        bill += quantities[i]*products[i]->getPrice();
+    }
+    if (discount) {
+        if (regime)
+            bill *= 0.95;
+        else
+            bill *= 0.98;
+        discount = false;
+    }
+    cout << "Amount to pay: " << bill << endl;
+
+    // Manage points
+    addPoints(bill);
+
+    // Appraisal
+    cout << "Your appraisal for the service (0-5): ";
+    cin >> appraisal;
+
+    addAppraisal(appraisal);
 }
--- AedaFirst/client.h
+++ AedaFirst/client.h
@@ -3,7 +3,10 @@
 #define CLIENT_H
 
 #include <iostream>
+#include "store.h"
+#include "product.h"
 #include <list>
+#include <vector>
 
 class Client {
 private:
@@ -11,6 +14,7 @@ private:
     unsigned nif;
     bool regime; // true if premium regime, false if normal regime
     unsigned points;
+    bool discount;
     bool opinion;
     std::list<int> appraisals;
     int sumOfAppraisals;
@@ -21,12 +25,14 @@ public:
     unsigned getNif() const;
     bool getRegime() const;
     unsigned getPoints() const;
+    bool getDiscount() const;
     bool getOpinion() const;
     void setName(std::string name);
     void setNif(unsigned nif);
     void setRegime(bool regime);
     void addAppraisal(int appraisal);
-
+    void addPoints(const float &bill);
+    void makeOrder(std::vector<Store*> stores);
 
 };
 #endif //CLIENT_H
