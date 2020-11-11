69d81204c9a796360b1e99c9855252597e3cd269
Pedro Pinheiro <64590120+PPinhas@users.noreply.github.com>
2020-11-11 17:05:01 +0000

    still implementing

--- AedaFirst/employee.cpp
+++ AedaFirst/employee.cpp
@@ -1,4 +1,43 @@
 //
 // Created by pedro on 11/11/2020.
 //
+#include "employee.h"
+#include "store.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
+
+Employee::Employee(name = "", nif = 0, store, salary = 0, numOrders = 0){
+    setName(name);
+    setNif(nif);
+    setStore(store);
+    setSalary(salary);
+    setNumOrders(numOrders);
+}
+
+string Employee::getName() const {return name;}
+
+unsigned int Employee::getNif() const {return nif;}
+
+store Employee::getStore() const {return store;}
+
+float Employee::getSalary() const {return salary;}
+
+unsigned int Employee::getNumOrders() const {return numOrders;)
+
+void Employee::setName(string name) {this->name = name;}
+
+void Employee::setNif(unsigned int nif){this->nif = nif;}
+
+void Employee::setStore(Store store){this->store = store;}
+
+void Employee::setSalary(float salary){this->salary = salary;}
+
+void Employee::setNumOrders(int numOrders){this->numOrder = numOrder;}
+
+void showEmployee() const{
+    cout << "Name: " << name << "NIF:" << nif;
+    showStore(store);
+    cout << "Salary: " << salary << "Number of orders:" << numOrders;
+}
--- AedaFirst/employee.h
+++ AedaFirst/employee.h
@@ -5,32 +5,32 @@
 #ifndef MAIN_CPP_WORKER_H
 #define MAIN_CPP_WORKER_H
 #include "store.h"
+#include <iostream>
 
 class Employee {
 private:
     std::string name;
     unsigned int nif;
     Store store;
-    unsigned int salary;
+    float salary;
     unsigned int numOrders
 public:
-    Employee();
-    Employee(std::string name, unsigned int nif, Store store, unsigned int salary, unsigned int numOrders);
+    Employee(std::string name, unsigned int nif, Store store, float salary, unsigned int numOrders);
     std::string getName() const;
     unsigned getNif() const;
     bool getStore() const;
-    unsigned getSalary() const;
-    bool getNumOrders() const;
+    float getSalary() const;
+    unsigned int getNumOrders() const;
     void setName(std::string name);
     void setNif(unsigned nif);
     void setStore(Store store);
-    void setSalary(unsigned int salary);
+    void setSalary(float salary);
     void setNumOrders(unsigned int numOrders);
     /**
      * Print employee in format:
      * Name: <EMPLOYEE_NAME> NIF: <EMPLOYEE_NIF>
      * Store ID: <STORE_ID> Store: <STORE_NAME>
-     * SALARY: <EMPLOYEE_SALARY> Number of orders: <NUM_ORDERS>
+     * Salary: <EMPLOYEE_SALARY> Number of orders: <NUM_ORDERS>
      */
     void showEmployee() const;
 
